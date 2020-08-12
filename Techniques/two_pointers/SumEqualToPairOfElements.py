1ab95c938b6e10223b88f9f1bac94e4c87226fdd
r3a10god <57110219+r3a10god@users.noreply.github.com>
2020-08-12 13:43:29 +0530

    Update SumEqualToPairOfElements.py

--- Techniques/two_pointers/SumEqualToPairOfElements.py
+++ Techniques/two_pointers/SumEqualToPairOfElements.py
@@ -1,21 +1,18 @@
+#  problem statement in the wiki (Techniques--Two pointers)
+
 def isPairSum(arr, n, x):
 	i, j = 0, n - 1
 
-	max_sum_pairs = f"{arr[0]} {arr[n - 1]}"
-	diff = x
-
 	while i < j:
 		current_sum = arr[i] + arr[j]
-		if abs(current_sum - x) <= diff:
-			max_sum_pairs = f"{arr[i]} {arr[j]}"
-			diff = abs(current_sum - x)
-		
+		if current_sum == x:
+			return True
+
 		if current_sum < x:
 			i += 1
 		else:
 			j -= 1
-
-	return max_sum_pairs
+	return False
 
 
 arr = list(map(int, input().split()))
