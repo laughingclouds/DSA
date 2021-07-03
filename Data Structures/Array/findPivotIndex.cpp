06d78337c365336c75c23522316b7f4bf01f3018
laughingclouds <zenithtaiga@gmail.com>
2021-07-03 21:41:41 +0530

    nothing

---
+++ Data Structures/Array/findPivotIndex.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+// using namespace std;
+
+class Solution {
+    public:
+        int pivotIndex(std::vector<int>& arr) {
+            int left = 0, n = arr.size();
+            int right = 0;
+
+            for (int i = i; i < n; i++) right += arr[i];
+
+            int i = 0;
+
+            while (left != right && i < n) {
+                left += arr[i];
+                right -= arr[i + 1];
+                i++;
+            }
+            return left == right ? i : -1;
+        }
+};
