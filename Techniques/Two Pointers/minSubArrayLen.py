5bf953ce9576ebc87fdc574488e3794075fd0ad7
laughingclouds <zenithtaiga@gmail.com>
2021-07-18 20:12:43 +0530

    nothing

--- Techniques/Two Pointers/minSubArrayLen.py
+++ Techniques/Two Pointers/minSubArrayLen.py
@@ -1,3 +1,37 @@
 class Solution:
     def minSubArrayLen(self, arr: list[int], t: int) -> int:
-        pass
+        n = len(arr)
+        prefixSumArr = [arr[0]] * n
+
+        for i in range(1, n):
+            s = prefixSumArr[i - 1] + arr[i]
+
+            if t <= s:
+                return i + 1
+            arr[i] = s
+        return n + 1
+
+    def returnPrefixSum(self, arr: list[int], t: int) -> int:
+        # tuple[int, list[int]]
+        n = len(arr)
+        sumArr = [arr[0]] * n
+        ans = n + 1
+
+        for i in range(1, n):
+            sumArr[i] = sumArr[i - 1] + arr[i]
+            if t <= sumArr[i]:
+                # ans = i + 1
+                return i + 1
+        # return ans, sumArr
+
+
+arr = [5, 1, 3, 4, 2]
+t = 10
+ans = Solution().returnPrefixSum(arr, t)
+
+# i = 0
+# for x in array:
+#     print(f"index: {i}\tvalue: {x}")
+#     i += 1
+
+print(ans)
